eb49b3c07e2c55395ef6a2e55ba9b901c5e2e25b
Ngoc901 <huanikii@gmail.com>
2025-02-04 22:11:00 +0100

    Simple UI test with Swing

---
+++ src/main/java/org/example/MatchCards.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.util.ArrayList;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class MatchCards {
+    MatchCards(){
+        System.out.println("match cards");
+    }
+}
