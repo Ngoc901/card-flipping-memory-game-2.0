6dcba81edd55c40e1874111db3b04ac20ce9d9ad
Ngoc901 <huanikii@gmail.com>
2025-07-07 08:21:45 +0200

    Scoring system

--- src/main/java/org/example/BonusCard.java
+++ src/main/java/org/example/BonusCard.java
@@ -1,4 +1,19 @@
 package org.example;
 
-public class BonusCard {
+import javax.swing.*;
+
+public class BonusCard extends Card {
+    BonusCard(String cardName, ImageIcon cardImageIcon) {
+        super(cardName, cardImageIcon);
+    }
+
+    @Override
+    public int getMismatchPenalty(){
+        return 500;
+    }
+
+    @Override
+    public int getMatchScore(){
+        return 50;
+    }
 }
