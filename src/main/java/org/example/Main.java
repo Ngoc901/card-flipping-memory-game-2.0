359ea72e257a778122b4d9aa096f48a1a80c614e
Ngoc901 <huanikii@gmail.com>
2025-07-08 12:33:12 +0200

    Some refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        MatchCards matchCards = new MatchCards();
-        matchCards.setupCard();
+//        MatchCards matchCards = new MatchCards();
+        UserManager userManager = new UserManager();
+        SwingUtilities.invokeLater(() -> new MainPage(userManager).createUI());
+
     }
 
 }
